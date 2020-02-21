e555c0174d0b4329d1d14584f060bcb28aba5061
dawsonckennedy <dawson.kennedy@mymail.champlain.edu>
2020-02-21 16:51:24 -0500

    Get to work with your new stuff

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.c
@@ -1,7 +1,9 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <stdio.h>
 int main()
 {
+	int test = foo(9000);
 
+	printf("\n %d \n", test);
 }
